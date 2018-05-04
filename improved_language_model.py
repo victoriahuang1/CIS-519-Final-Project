1a2d71159cd41ad16a7047292e07998cb9674bc7
Victoria Huang <vichuang@seas.upenn.edu>
2018-05-03 23:29:18 -0400

    commented stuff out

--- improved_language_model.py
+++ improved_language_model.py
@@ -92,12 +92,12 @@ if __name__ == '__main__':
     if len(sys.argv) > 3:
         pred_fam = True
 
-    print('Loading Wiki Train Data...')
-    train_data = tools.load_wiki_data(lm.wiki_path + 'train/')
-    train_docs, train_labels = train_data
-    print('Loading Wiki Test Data...')
-    test_data = tools.load_wiki_data(lm.wiki_path + 'test/')
-    test_docs, gold_labels = test_data
+    # print('Loading Wiki Train Data...')
+    # train_data = tools.load_wiki_data(lm.wiki_path + 'train/')
+    # train_docs, train_labels = train_data
+    # print('Loading Wiki Test Data...')
+    # test_data = tools.load_wiki_data(lm.wiki_path + 'test/')
+    # test_docs, gold_labels = test_data
 
     print('Loading Gutenberg Train Data...')
     train_data_2 = tools.load_gutenberg_data(lm.gutenberg_path + 'train/')
@@ -111,89 +111,86 @@ if __name__ == '__main__':
     print('Training language models...')
 
     if vote:
-        ensemble = Ensemble(min_order, max_order, learn_fam=pred_fam)
-        ensemble.train(train_data)
+        # ensemble = Ensemble(min_order, max_order, learn_fam=pred_fam)
+        # ensemble.train(train_data)
         
         ensemble_2 = Ensemble(min_order, max_order, learn_fam=pred_fam)
         ensemble_2.train(train_data_2)
 
         print('Making Predictions...')
-        y_pred = predict_by_vote(ensemble, test_docs)
+        # y_pred = predict_by_vote(ensemble, test_docs)
         y_pred_2 = predict_by_vote(ensemble_2, test_docs_2)
     else:
         # clf = linear_model.SGDClassifier()
         clf = SVC()
         clf_2 = SVC()
         
-        sample_size = 500
+        sample_size = 200
 
-        indices = np.arange(len(train_docs))
-        sample = np.random.choice(indices, size=sample_size, replace=False)
-
-        train_docs = [train_docs[i] for i in sample]
-        train_labels = [train_labels[i] for i in sample]
+        # indices = np.arange(len(train_docs))
+        # sample = np.random.choice(indices, size=sample_size, replace=False)
+        #
+        # train_docs = [train_docs[i] for i in sample]
+        # train_labels = [train_labels[i] for i in sample]
 
         indices_2 = np.arange(len(train_docs_2))
-        sample_2 = np.random.choice(indices_2, size=max(sample_size, indices_2), replace=False)
-
-        train_docs = [train_docs[i] for i in sample]
-        train_labels = [train_labels[i] for i in sample]
+        sample_2 = np.random.choice(indices_2, size=sample_size, replace=False)
         
         train_docs_2 = [train_docs_2[i] for i in sample_2]
         train_labels_2 = [train_labels_2[i] for i in sample_2]
         
         if pred_fam:
-            for i in range(0, len(train_docs)):
-                train_labels[i] = lm.lang_to_fam[train_labels[i]]
+            # for i in range(0, len(train_docs)):
+            #     train_labels[i] = lm.lang_to_fam[train_labels[i]]
                 
             for i in range(0, len(train_docs_2)):
                 train_labels_2[i] = lm.lang_to_fam[train_labels_2[i]]
 
-        indices = np.arange(len(test_docs))
-        sample = np.random.choice(indices, size=sample_size, replace=False)
+        # indices = np.arange(len(test_docs))
+        # sample = np.random.choice(indices, size=sample_size, replace=False)
 
         indices_2 = np.arange(len(test_docs_2))
-        sample_2 = np.random.choice(indices_2, size=max(sample_size, indices_2), replace=False)
+        sample_2 = np.random.choice(indices_2, size=sample_size, replace=False)
         
-        test_docs = [test_docs[i] for i in sample]
-        gold_labels = [gold_labels[i] for i in sample]
+        # test_docs = [test_docs[i] for i in sample]
+        # gold_labels = [gold_labels[i] for i in sample]
         
         test_docs_2 = [test_docs_2[i] for i in sample_2]
-        gold_labels_2 = [gold_labels[i] for i in sample_2]
+        gold_labels_2 = [gold_labels_2[i] for i in sample_2]
 
         n = 3
         orders = {}
         for lang in lm.lang_to_fam.keys():
             orders[lang] = n
 
-        lms = lm.get_language_models(orders, 1, pred_fam, train_data, True)
+        # lms = lm.get_language_models(orders, 1, pred_fam, train_data, True)
         lms_2 = lm.get_language_models(orders, 1, pred_fam, train_data_2, True)
         
         print('Getting features...')
-        train_x = get_lm_as_features(lms, train_docs)
-        test_x = get_lm_as_features(lms, test_docs, is_train=False)
+        # train_x = get_lm_as_features(lms, train_docs)
+        # test_x = get_lm_as_features(lms, test_docs, is_train=False)
         
         train_x_2 = get_lm_as_features(lms_2, train_docs_2)
         test_x_2 = get_lm_as_features(lms_2, test_docs_2)
 
         print('Fitting linear model...')
-        train_y = np.array(train_labels)
-        clf.fit(train_x, train_y)
+        # train_y = np.array(train_labels)
+        # clf.fit(train_x, train_y)
 
         train_y_2 = np.array(train_labels_2)
         clf_2.fit(train_x_2, train_y_2)
         
         print('Making Predictions...')
-        y_pred = clf.predict(test_x)
+        # y_pred = clf.predict(test_x)
         
-        y_pred = clf_2.predict(test_x_2)
+        y_pred_2 = clf_2.predict(test_x_2)
 
     if pred_fam:
-        for i in range(0, len(gold_labels)):
-            gold_labels[i] = lm.lang_to_fam[gold_labels[i]]
+        # for i in range(0, len(gold_labels)):
+        #     gold_labels[i] = lm.lang_to_fam[gold_labels[i]]
         
         for i in range(0, len(gold_labels_2)):
             gold_labels_2[i] = lm.lang_to_fam[gold_labels_2[i]]
 
-    tools.write_pred(y_pred, gold_labels, sys.argv[1])
+    # tools.write_pred(y_pred, gold_labels, sys.argv[1])
     tools.write_pred(y_pred_2, gold_labels_2, (sys.argv[1] + '_1'))
