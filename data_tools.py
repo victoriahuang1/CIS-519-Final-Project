2e82301936ab891a1220bac4269729d1dd17dc50
Sam Davis <davsam@seas.upenn.edu>
2018-05-04 17:37:04 -0400

    fixed loading gutenberg

--- data_tools.py
+++ data_tools.py
@@ -13,33 +13,28 @@ def read_lang_to_fam(filepath):
 
 
 # Reads a wiki file and returns a list of documents
-def read_wiki_file(datapath, filtered=False):
+def read_wiki_file(datapath):
     docs = []
 
     with open(datapath, mode='r', errors='ignore', encoding='utf-8') as f:
         curr_doc = []
         for line in f:
             if '</doc>' in line:
-                if len(curr_doc) > 0:
-                    docs.append(curr_doc)
+                docs.append(curr_doc)
                 curr_doc = []
                 continue
             elif '<doc' in line:
                 continue
             words = line.split()
-
-            if filtered:
-                filtered_words = []
-                for word in words:
-                    if not word.isdigit():
-                        filtered_words.append(word)
-                words = filtered_words
             curr_doc += words
     f.close()
     if len(curr_doc) > 0:
         docs.append(curr_doc)
     return docs
 
+def read_gutenberg_file(datapath):
+    f = open(datapath, "r", errors='ignore', encoding='utf-8')
+    return f.read()
 
 # Writes all characters in train and test set to file
 def get_all_chars(train_dir, test_dir, out_path):
@@ -80,24 +75,23 @@ def load_all_chars(char_file):
 
 
 # Loads examples (lists of lists of strings) and their labels
-def load_wiki_data(data_dir, filtered=False):
+def load_wiki_data(data_dir):
     examples = []
     labels = []
     for subdir in os.listdir(data_dir):
         for doc in os.listdir(data_dir + subdir):
-            new_docs = read_wiki_file(data_dir + subdir + '/' + doc, filtered=False)
+            new_docs = read_wiki_file(data_dir + subdir + '/' + doc)
             for i in range(len(new_docs)):
                 labels.append(subdir)
             examples += new_docs
     return examples, labels
-
 
 def load_gutenberg_data(data_dir):
     examples = []
     labels = []
     for subdir in os.listdir(data_dir):
         for doc in os.listdir(data_dir + subdir):
-            examples.append(doc)
+            examples.append(read_gutenberg_file(data_dir + subdir + '/' + doc))
             labels.append(subdir)
     return examples, labels
 
