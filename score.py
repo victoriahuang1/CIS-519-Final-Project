10cf6a9173ce43e46b7fe3e9d0fa32ada72607d9
Victoria Huang <vichuang@seas.upenn.edu>
2018-05-07 02:37:30 -0400

    added family prediction

--- score.py
+++ score.py
@@ -1,15 +1,21 @@
 import sys
+import language_model
 
 
 # Loads the labels from file into array
-def load_labels(filepath):
+def load_labels(filepath, pred_fam=False):
     y_pred = []
     gold_labels = []
     with open(filepath, mode='r', errors='ignore') as f:
         for line in f:
             words = line.split()
-            y_pred.append(words[0].strip().lower())
-            gold_labels.append(words[1].strip().lower())
+            pred = words[0].strip().lower()
+            gold = words[1].strip().lower()
+            if pred_fam:
+                pred = language_model.lang_to_fam[pred]
+                gold = language_model.lang_to_fam[gold]
+            y_pred.append(pred)
+            gold_labels.append(gold)
     f.close()
     return y_pred, gold_labels
 
@@ -24,6 +30,6 @@ if __name__ == '__main__':
         print("Expecting paths to predictions file")
         sys.exit(1)
 
-    y_pred, gold_labels = load_labels(sys.argv[1])
+    y_pred, gold_labels = load_labels(sys.argv[1], pred_fam=True)
 
     print("Accuracy: {}".format(get_accuracy(y_pred, gold_labels)))
